b8d4f9d8c68c52940aec2d4ee10dae4ec02b5c09
aj1s <ajones@tnc.org>
2021-01-30 01:45:17 -0700

    (1) added 'detach package' fn; (2) increased java memory allocation for read/write of xls files
    Function 'detach_package' defined then applied to pkgs "XLConnect" and "rJava". It is necessary to remove these temporarily in order to allocate required memory for 'XLConnect' and its dependency 'rJava'.  These packages are re-installed after the memory allocation adjustment.

--- plot_caland.r
+++ plot_caland.r
@@ -6,7 +6,16 @@
 # California, through Lawrence Berkeley National Laboratory (subject to 
 # receipt of any required approvals from the U.S. Dept. of Energy).  All 
 # rights reserved.
-# If you have questions about your rights to use or distribute this software, # please contact Berkeley Lab's Intellectual Property Office at # IPO@lbl.gov. #  # NOTICE.  This Software was developed under funding from the U.S. Department # of Energy and the U.S. Government consequently retains certain rights.  As # such, the U.S. Government has been granted for itself and others acting on # its behalf a paid-up, nonexclusive, irrevocable, worldwide license in the # Software to reproduce, distribute copies to the public, prepare derivative  # works, and perform publicly and display publicly, and to permit others to do so.
+# If you have questions about your rights to use or distribute this software,
+# please contact Berkeley Lab's Intellectual Property Office at
+# IPO@lbl.gov.
+# 
+# NOTICE.  This Software was developed under funding from the U.S. Department
+# of Energy and the U.S. Government consequently retains certain rights.  As
+# such, the U.S. Government has been granted for itself and others acting on
+# its behalf a paid-up, nonexclusive, irrevocable, worldwide license in the
+# Software to reproduce, distribute copies to the public, prepare derivative 
+# works, and perform publicly and display publicly, and to permit others to do so.
 ####
 
 # This software and its associated input data are licensed under a modified BSD open source license
@@ -96,8 +105,28 @@
 # setwd("<your_path>/caland/")
 setwd("./")
 
-# this enables java to use up to 4GB of memory for reading and writing excel files
-options(java.parameters = "-Xmx8g" )
+#Define function 'detach_package' 
+detach_package <- function(pkg, character.only = FALSE)
+{
+  if(!character.only)
+  {
+    pkg <- deparse(substitute(pkg)) 
+  }
+  search_item <- paste("package", pkg, sep = ":")
+  while(search_item %in% search())
+  {
+    detach(search_item, unload = TRUE, character.only = TRUE)
+  }
+}
+
+detach_package("XLConnect")
+detach_package("rJava")
+
+# this enables java to use up to 16GB of memory for reading and writing excel files
+options(java.parameters = "-Xmx32g" )  # originally set to "-Xmx8g" 
+
+# Install of required packages, including re-install of those detached after greater allocation of memory for Java 
+install.packages(c("XLConnect", "rJava"))
 
 # Load all the required packages
 libs <- c( "XLConnect", "ggplot2", "grid", "RColorBrewer", "reshape2")
