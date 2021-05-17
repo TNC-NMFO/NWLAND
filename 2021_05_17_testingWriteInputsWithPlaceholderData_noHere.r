c9e441fefd649516091718597651833a9567d970
Steve Bassett <sbassett@tnc.org>
2021-05-17 16:06:50 -0600

    Revert "Revert "Runs files""
    This reverts commit 668597dceedcf11be1127d1a1ca31f13ecab4722.

---
+++ 2021_05_17_testingWriteInputsWithPlaceholderData_noHere.r
@@ -0,0 +1,35 @@
+#load XLconnect
+library(XLConnect)
+
+#load the write_caland_inputs function to memory
+source("write_caland_inputs.r")
+
+write_caland_inputs(
+    c_file = "2021_05_14_carbon_input_nwland_testN.xls", # output file name
+    inputs_dir = "2021_05_14_carbon_input_nwland_testN", # output file directory
+    parameter_file = "proto_lc_params.xls",
+    scenarios_file = "proto_nwl_scenarios_v01_ha.xls",
+    units_scenario = "ha", # can be "ac" or "ha", we use hectares for NWLAND
+    climate_c_file = "climate_c_scalars_nmco_null_2010_2100.csv", 
+    fire_area_file = "fire_area_RDS-2020-0016_averageannual_2001-2100.csv",
+    mortality_file = "mortality_annual_testing.csv",
+    area_gis_files_new = "NWLAND_Area_Changes_2015_to_2050.csv",
+    land_change_method = "Landuse_Avg_Annual",
+    area_gis_files_orig = c("area_lab_sp9_own9_2001lt15_sqm_stats.csv", "area_lab_sp9_own9_2010lt15_sqm_stats.csv"),
+    carbon_gis_files = c("soc_tpha_2017_stats.csv",
+        "inventory_proto_agc_tpha.csv", 
+        "inventory_proto_bgc_tpha.csv", 
+        "inventory_proto_ddc_tpha.csv", 
+        "inventory_proto_dsc_tpha.csv", 
+        "inventory_proto_ltc_tpha.csv", 
+        "inventory_proto_usc_tpha.csv"),
+    scen_tag = "protoA",
+    start_year = 2016,
+    end_year = 2050,
+    CLIMATE = "HIST",
+    forest_mort_fact = 2, #what does this do?
+    forest_mort_adj_first = 2015,
+    forest_mort_adj_last = 2024,
+    control_wildfire_lulcc_file = "individual_proposed_sims_control_lulcc_wildfire_aug2018.csv", #not used becuse following line is false
+    control_wildfire_lulcc = FALSE
+)
