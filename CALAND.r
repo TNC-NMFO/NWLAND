5c7c908c8197edbfcfbc4cea212888f090538169
aj1s <ajones@tnc.org>
2021-05-26 03:06:08 -0600

    Comment out java.parameters specification in CALAND(), given fa1fcd7bbb73d334df439f11ed7bf0aa45b1e519
    This change may be unnecessary.

--- CALAND.r
+++ CALAND.r
@@ -6,7 +6,16 @@
 # California, through Lawrence Berkeley National Laboratory (subject to 
 # receipt of any required approvals from the U.S. Dept. of Energy).  All 
 # rights reserved.
-# If you have questions about your rights to use or distribute this software, # please contact Berkeley Lab's Intellectual Property Office at # IPO@lbl.gov. #  # NOTICE.  This Software was developed under funding from the U.S. Department # of Energy and the U.S. Government consequently retains certain rights.  As # such, the U.S. Government has been granted for itself and others acting on # its behalf a paid-up, nonexclusive, irrevocable, worldwide license in the # Software to reproduce, distribute copies to the public, prepare derivative  # works, and perform publicly and display publicly, and to permit others to do so.
+# If you have questions about your rights to use or distribute this software,
+# please contact Berkeley Lab's Intellectual Property Office at
+# IPO@lbl.gov.
+# 
+# NOTICE.  This Software was developed under funding from the U.S. Department
+# of Energy and the U.S. Government consequently retains certain rights.  As
+# such, the U.S. Government has been granted for itself and others acting on
+# its behalf a paid-up, nonexclusive, irrevocable, worldwide license in the
+# Software to reproduce, distribute copies to the public, prepare derivative 
+# works, and perform publicly and display publicly, and to permit others to do so.
 ####
 
 # This software and its associated input data are licensed under a modified BSD open source license
@@ -199,7 +208,7 @@
 ################################################# Start script ############################################################
 
 # this enables java to use up to 4GB of memory for reading and writing excel files
-options(java.parameters = "-Xmx4g" )
+# options(java.parameters = "-Xmx4g" )
 
 # Load all the required packages
  libs <- c( "XLConnect" )
