1ced110e65f6831acdbde8054af92cee79da0af7
Steve Bassett <sbassett@tnc.org>
2021-07-06 11:25:27 -0600

    As ran on COGEOPROC
    #72 addressed through manually renaming the `outdir` between running `CALAND()` and `plot_CALAND()`. Still needs to be polished, but it runs!

---
+++ 2021_07_04_testingCALAND.R
@@ -0,0 +1,26 @@
+# this enables java to use up to 16GB of memory for reading and writing excel files
+options(java.parameters = "-d64 -Xms50g -Xmx200g" )  # originally set to "-Xmx8g" 
+
+#load XLConnect (which should also load its dependency rJava)
+library(XLConnect)
+
+#load the write_caland_inputs function to memory
+source("CALAND.r")
+
+CALAND(
+  scen_file_arg = "NWL_Proto_v1_Alt_A_protoA_hist.xls", 
+  c_file_arg = "2021_05_14_carbon_input_nwland_testB3_devAll_noOcean.xls", 
+  indir = "2021_05_14_carbon_input_nwland_testB3_devAll_noOcean",
+  outdir = "2021_05_14_carbon_input_nwland_testB3_devAll_noOcean", 
+  start_year = 2016, 
+  end_year = 2051, 
+  value_col_dens = 7, 
+  ADD_dens = TRUE, 
+  value_col_accum = 7, 
+  ADD_accum = TRUE, 
+  value_col_soilcon=8, 
+  ADD_soilcon = TRUE, 
+  NR_Dist = 120, 
+  WRITE_OUT_FILE = TRUE, 
+  blackC = FALSE
+)
